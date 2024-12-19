885085b0d6c8905978262ddb4954d43b35986b36
Olivier Cailloux <olivier.cailloux@gmail.com>
2024-12-19 19:50:59 +0100

    import

--- src/main/java/io/github/oliviercailloux/jsand/common/ReadyService.java
+++ src/main/java/io/github/oliviercailloux/jsand/common/ReadyService.java
@@ -1,7 +1,8 @@
 package io.github.oliviercailloux.jsand.common;
 
 import java.rmi.Remote;
+import java.rmi.RemoteException;
 
 public interface ReadyService extends Remote {
-  public void ready() throws java.rmi.RemoteException;
+  public void ready() throws RemoteException;
 }
